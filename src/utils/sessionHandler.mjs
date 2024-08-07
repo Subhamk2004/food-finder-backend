b6c764455098078af6755b1a7cb15d7b20175958
Subhamk2004 <subhamk1978@gmail.com>
2024-08-08 00:54:01 +0530

    changes

--- src/utils/sessionHandler.mjs
+++ src/utils/sessionHandler.mjs
@@ -4,12 +4,10 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import MongoStore from 'connect-mongo';
 import express from 'express';
-import dotenv from 'dotenv';
 
-dotenv.config();
+let databaseName = 'FoodFinder'
+let mongoURI = `mongodb+srv://subhamrahar22:HsnrYatHXhtSeKzw@foodfinder.xrb4b70.mongodb.net/${databaseName}`;
 
-let mongoURI = process.env.MONGO_URI;
-let cookieSecret = process.env.COOKIE_SECRET;
 
 let router = express();
 const mongoOptions = {
@@ -19,48 +17,50 @@ const mongoOptions = {
   tlsInsecure: false,
   // tlsAllowInvalidCertificates: true
 };
-
-mongoose.connect(mongoURI, mongoOptions)
-  .then((data) => {
-    console.log('Connected to MongoDB successfully');
-    setupMiddleware();
-  })
-  .catch((error) => {
-    console.log('Failed to connect to MongoDB: ', error);
-  })
-
-
-
-function setupMiddleware() {
-  router.use(express.json());
-  router.use(cookieParser(cookieSecret));
-  router.use(
-    session({
-      secret: cookieSecret,
-      saveUninitialized: false,
-      resave: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
-      },
-      store: MongoStore.create({
-        mongoUrl: mongoURI,
-        ttl: 14 * 24 * 60 * 60 // 14 days
-      }),
+let mongoConnect = () => {
+  mongoose.connect(mongoURI, mongoOptions)
+    .then((data) => {
+      console.log('Connected to MongoDB successfully');
     })
-  );
-
-  router.use(passport.initialize());
-  router.use(passport.session());
-
-  router.get('/', (req, res) => {
-    console.log(req.session);
-    console.log(req.sessionID);
-    req.session.visited = true;
-    res.status(200).send('Hello from the home route!');
-  });
+    .catch((error) => {
+      console.log('Failed to connect to MongoDB: ', error);
+    })
 }
+
+mongoConnect();
+
+
+router.use(express.json());
+
+router.use(cookieParser('CookieSecret'));
+router.use(
+  session({
+    secret: "FoodFinderCookieSecretComplex",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // true in production
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+      // 7 days
+    },
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    }),
+  })
+)
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/', (req, res) => {
+  console.log(req.session);
+  console.log(req.sessionID);
+  req.session.visited = true;
+
+
+  res.status(200).send('Hello from the home route!');
+})
 
 export default router;
